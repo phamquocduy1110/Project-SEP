3e9bfaca4db6864931a9770d665d886356e77b46
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2021-05-29 08:02:01 +0700

    - Create AdminIndex Page - Create AdminTable Page - Add css for them

---
+++ VanLangFeedBackWebsite/VanLangFeedBackWebsite/Controllers/AdminHomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanLangFeedBackWebsite.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+
+        public ActionResult AdminTable()
+        {
+            return View();
+        }
+    }
+}
