21364af73eba0ded0265b20ab19c425f216e1b07
Sakura Ayame Kinomoto <55733660+phamquocduy1110@users.noreply.github.com>
2021-06-04 14:41:37 +0700

    Create Questions Page for users

--- dieubanthuonghoi/dieubanthuonghoi/Areas/Admin/AdminAreaRegistration.cs
+++ dieubanthuonghoi/dieubanthuonghoi/Areas/Admin/AdminAreaRegistration.cs
@@ -15,7 +15,7 @@ namespace dieubanthuonghoi.Areas.Admin
         public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
-                "Admin_default",
+                "Admin",
                 "Admin/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
