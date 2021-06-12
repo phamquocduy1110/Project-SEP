1eb7ce49852f57b2f8f45ce767121e90e522836e
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-06-13 03:55:47 +0700

    - Add Database Model - Code function load all product onto list

--- dieubanthuonghoi/dieubanthuonghoi/Areas/Admin/Controllers/ListQuestionsAdminController.cs
+++ dieubanthuonghoi/dieubanthuonghoi/Areas/Admin/Controllers/ListQuestionsAdminController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dieubanthuonghoi.Models;
 
 namespace dieubanthuonghoi.Areas.Admin.Controllers
 {
     public class ListQuestionsAdminController : Controller
     {
+        QUANLYCANTEENEntities model = new QUANLYCANTEENEntities();
+        
         // GET: Admin/ListQuestionsAdmin
         public ActionResult Index()
         {
-            return View();
+            var product = model.FOODs.ToList().OrderByDescending(x => x.ID).ToList();
+            return View(product);
         }
     }
 }
