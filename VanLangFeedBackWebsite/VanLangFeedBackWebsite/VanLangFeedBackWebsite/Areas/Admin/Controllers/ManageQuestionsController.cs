9c967bbfb715f73859c195b1439d1627eeea0289
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-06-29 15:31:38 +0700

    Code function load all list questions from database

--- VanLangFeedBackWebsite/VanLangFeedBackWebsite/VanLangFeedBackWebsite/Areas/Admin/Controllers/ManageQuestionsController.cs
+++ VanLangFeedBackWebsite/VanLangFeedBackWebsite/VanLangFeedBackWebsite/Areas/Admin/Controllers/ManageQuestionsController.cs
@@ -15,12 +15,37 @@ namespace VanLangFeedBackWebsite.Areas.Admin.Controllers
 {
     public class ManageQuestionsController : Controller
     {
+        DIEUBANTHUONGHOIWEBSITEEntities model = new DIEUBANTHUONGHOIWEBSITEEntities();
 
-        // GET: Admin/AdminManageQuestions
+        // GET: Load All List Questions / AdminManageQuestions
         public ActionResult Index()
         {
-/*            var question = model.QUESTIONS.ToList().OrderByDescending(x => x.ID).ToList();
-*/            return View();
+            var question = model.CAU_HOI.ToList().OrderByDescending(x => x.ID).ToList();
+            return View(question);
+        }
+
+        // GET: DANH_MUC / AdminManageQuestions
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.question_type = model.DANH_MUC.OrderByDescending(x => x.ID).ToList();
+            ViewBag.account_type = model.TAI_KHOAN.OrderByDescending(x => x.ID).ToList();
+            return View();
+        }
+
+        // POST: CAU_HOI / AdminManageQuestions
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Create(CAU_HOI f, HttpPostedFileBase IMAGE_URL)
+        {
+            var question = new CAU_HOI();
+
+            question.TEN_CAU_HOI = f.TEN_CAU_HOI;
+            question.MO_TA = f.MO_TA;
+            question.ID_DANH_MUC = f.ID_DANH_MUC;
+            question.NGUOI_DANG = f.NGUOI_DANG;
+            model.CAU_HOI.Add(question);
+            model.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
