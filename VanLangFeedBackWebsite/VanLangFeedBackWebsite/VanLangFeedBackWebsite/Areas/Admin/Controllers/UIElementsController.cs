b83b570dc302d7d693bfc81e5fa410936cf923d0
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-06-30 11:04:28 +0700

    - Code function Create Questions

---
+++ VanLangFeedBackWebsite/VanLangFeedBackWebsite/VanLangFeedBackWebsite/Areas/Admin/Controllers/UIElementsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VanLangFeedBackWebsite.Areas.Admin.Controllers
+{
+    public class UIElementsController : Controller
+    {
+        // GET: Admin/AdminUIElements
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
