ea8d3fb044cc15de4d70610ed6fd5f9005362179
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-07-02 18:11:26 +0700

    Update Pagination for question list in user page for search tool

--- VanLangFeedBackWebsite/VanLangFeedBackWebsite/VanLangFeedBackWebsite/Controllers/QuestionsController.cs
+++ VanLangFeedBackWebsite/VanLangFeedBackWebsite/VanLangFeedBackWebsite/Controllers/QuestionsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using VanLangFeedBackWebsite.Models;
 
 namespace VanLangFeedBackWebsite.Controllers
@@ -12,19 +13,55 @@ namespace VanLangFeedBackWebsite.Controllers
         DIEUBANTHUONGHOIWEBSITEEntities model = new DIEUBANTHUONGHOIWEBSITEEntities();
 
         [AllowAnonymous]
-        public ActionResult Search(string keyword)
+        public ActionResult Search(string keyword, int? page)
         {
-            var search = model.CAU_HOI.ToList();
-            search = search.Where(x => x.CAU_HOI1.ToLower().Contains(keyword.ToLower())).ToList();
+            var pageNumber = page ?? 1;
+            var pageSize = 10;
+
+            var search = model.CAU_HOI.OrderByDescending(x => x.ID).Where(x => x.CAU_HOI1.ToLower().Contains(keyword.ToLower())).ToPagedList(pageNumber, pageSize);
             ViewBag.keyword = keyword;
             return View("Index", search);
         }
 
         // GET: Questions
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            var question = model.CAU_HOI.ToList();
-            return View(question);
+            var pageNumber = page ?? 1;
+            var pageSize = 3;
+
+            var quesionList = model.CAU_HOI.OrderByDescending(x => x.ID).ToPagedList(pageNumber, pageSize);
+            return PartialView(quesionList);
         }
+
+        public PartialViewResult CategoryPartical(int ? page)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 3;
+
+            var categoryList = model.DANH_MUC.OrderByDescending(x => x.DANH_MUC1).ToPagedList(pageNumber, pageSize);
+            return PartialView(categoryList);
+        }
+
+        /*public ActionResult QuestionListPartical(int? page, int? category)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 10;
+
+            if (category != null)
+            {
+                var quesionList = model.CAU_HOI
+                                .OrderByDescending(x => x.ID)
+                                .Where(x => x.ID_DANH_MUC == category)
+                                .ToPagedList(pageNumber, pageSize);
+                return PartialView(quesionList);
+            }
+            else
+            {
+                var quesionList = model.CAU_HOI
+                                .OrderByDescending(x => x.ID)
+                                .ToPagedList(pageNumber, pageSize);
+                return PartialView(quesionList);
+            }
+        }*/
     }
 }
